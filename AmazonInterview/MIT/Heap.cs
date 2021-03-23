c95e11133485c9bee8a1937d2d84e081869586b6
marek <mmalek06@gmail.com>
2021-03-23 09:42:54 +0100

    heap sort

--- AmazonInterview/MIT/Heap.cs
+++ AmazonInterview/MIT/Heap.cs
@@ -1,26 +1,58 @@
 ﻿namespace AmazonInterview.MIT {
+    /// <summary>
+    /// var h = new Heap(new[] { 4, 1, 3, 2, 16, 9, 10, 14, 8, 7 });
+    ///            
+    /// h.BuildMaxHeap();
+    /// h.HeapSort();
+    /// </summary>
     public class Heap {
         private int[] _heap;
+        private int _heapSize;
 
-        public Heap(int[] arr) =>
+        public Heap(int[] arr) {
             _heap = arr;
+            _heapSize = arr.Length;
+        }
 
         public void BuildMaxHeap() {
-            for (var i = _heap.Length / 2; i >= 0; i--)
+            for (var i = _heapSize / 2; i >= 0; i--)
                 MaxHeapify(i);
+        }
+
+        public int[] HeapSort() {
+            var arr = new int[_heap.Length];
+            var idx = _heap.Length - 1;
+
+            while (_heapSize > 0) {
+                arr[idx] = ExtractMax();
+                idx--;
+            }
+
+            return arr;
+        }
+
+        public int ExtractMax() {
+            var max = _heap[0];
+
+            _heap[0] = _heap[_heapSize - 1];
+            _heapSize--;
+
+            MaxHeapify(0);
+
+            return max;
         }
 
         private void MaxHeapify(int i) {
             var left = 2 * i;
             var right = 2 * i + 1;
-            var largest = -1;
+            int largest;
 
-            if (left < _heap.Length && _heap[left] > _heap[i])
+            if (left < _heapSize && _heap[left] > _heap[i])
                 largest = left;
             else
                 largest = i;
 
-            if (right < _heap.Length && _heap[right] > _heap[largest])
+            if (right < _heapSize && _heap[right] > _heap[largest])
                 largest = right;
 
             if (largest != i) {
--- AmazonInterview/Program.cs
+++ AmazonInterview/Program.cs
@@ -3,7 +3,7 @@
 namespace AmazonInterview {
     class Program {
         static void Main(string[] args) {
-            new Heap(new[] { 1, 2, 3, 4, 5 }).BuildMaxHeap();
+
         }
     }
 }

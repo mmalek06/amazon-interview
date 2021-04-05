22d8a0875d95c4ca48106ce588c628bfdc20ca7a
marek <mmalek06@gmail.com>
2021-04-05 09:57:20 +0200

    up

---
+++ AmazonInterview/MIT/CountingSort.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace AmazonInterview.MIT {
+    public static class CountingSort {
+        public static int[] Go(int[] arr) {
+            var k = -1;
+
+            for (var i = 0; i < arr.Length; i++) {
+                if (arr[i] > k)
+                    k = arr[i];
+            }
+
+            var counts = new List<int>[k + 1];
+
+            for (var i = 0; i < arr.Length; i++) {
+                if (counts[arr[i]] is null)
+                    counts[arr[i]] = new List<int>(k);
+
+                counts[arr[i]].Add(arr[i]);
+            }
+
+            var result = new int[arr.Length];
+            var resultIndex = 0;
+
+            foreach (var row in counts) {
+                if (row is null)
+                    continue;
+
+                for (var i = 0; i < row.Count; i++) {
+                    result[resultIndex] = row[i];
+                    resultIndex++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- AmazonInterview/Program.cs
+++ AmazonInterview/Program.cs
@@ -1,7 +1,9 @@
-﻿namespace AmazonInterview {
+﻿using AmazonInterview.MIT;
+
+namespace AmazonInterview {
     class Program {
         static void Main(string[] args) {
-
+            CountingSort.Go(new[] { 1, 3, 1, 2, 5 });
         }
     }
 }
